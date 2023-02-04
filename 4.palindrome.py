38448c56d8c5ce2be2bdb03675dc05e47d095c12
Karthi347 <122214206+Karthi347@users.noreply.github.com>
2023-02-04 14:38:05 +0530

    Day 3

--- 4.palindrome.py
+++ 4.palindrome.py
@@ -1,11 +1,12 @@
-n=int(input("Enter a number: "))
-temp=n
-reverse=0
-while(n>0):
-    digit=n%10
-    reverse=reverse*10+digit
-    n=n//10
-if(temp==reverse):
-    print("The number is a palindrome")
+def reverse(s):
+    str=""
+    for i in s:
+        str=i+str
+    return str
+
+#main
+s=input("Enter a string/number: ")
+if reverse(s)==s:
+    print(True)
 else:
-    print("The number is not a palindrome")
+    print(False)
